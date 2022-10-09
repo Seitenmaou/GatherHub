401495203077911d80bc78fd511464b320e4e76a
Euri Tanaka <dev@mugendiary.com>
2022-10-09 16:41:41 -0700

    check failure on differnt system

---
+++ backend/index.js
@@ -0,0 +1,32 @@
+// Modules and Globals
+require('dotenv').config()
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const path = require('path')
+const app = express()
+const defineCurrentUser = require('./middleware/defineCurrentUser')
+
+// Express Settings
+app.use(cors())
+app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+app.use(defineCurrentUser)
+
+// serve static frontend in production mode
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, 'public', 'build')));
+}
+
+// Controllers & Routes
+
+app.use(express.urlencoded({ extended: true }))
+
+//app.use('/places', require('./controllers/places'))
+app.use('/users', require('./controllers/users'))
+
+// Listen for Connections
+app.listen(process.env.PORT, () => {
+    console.log(`Listening on ${process.env.PORT}`)
+})
